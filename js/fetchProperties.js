236d1a37fd247b0affc2b80cefcede0121a6d982
Robin Scavo <sirscavo@gmail.com>
2022-03-22 17:10:11 -0600

    revert changes

--- js/fetchProperties.js
+++
@@ -1,8 +0,0 @@
-const fetchProperties = async () => {
-    let uri = 'http://localhost:3000/properties';
-
-    const res = await fetch(uri);
-    const properties = await res.json();
-    console.log(properties);
-    return properties;
-}
